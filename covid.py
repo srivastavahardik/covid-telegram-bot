c85347cb4585ee87c39e89e60505e2eb9f8ec3bd
oddlyspaced <oddlyspaced1@gmail.com>
2021-04-24 21:53:27 +0530

    covid: Setup basic webdriver instance

---
+++ covid.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Setting up Chrome options
+option = webdriver.ChromeOptions()
+# For ChromeDriver version 79.0.3945.16 or over
+# Hide automation
+option.add_argument('--disable-blink-features=AutomationControlled')
+#Open Browser
+driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',options=option)
+
+# TODO : Replace with custom link
+# Reference link
+link = "https://twitter.com/search?q=verified+lucknow+%28bed+OR+beds+OR+icu+OR+oxygen+OR+ventilator+OR+ventilators%29+-%22not+verified%22+-%22unverified%22+-%22needed%22+-%22need%22+-%22needs%22+-%22required%22+-%22require%22+-%22requires%22+-%22requirement%22+-%22requirements%22&f=live"
+
+# Opening search results
+driver.get(link)
