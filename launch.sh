2bdc786eab5d99a6117e84a94999134e4f29fe83
oddlyspaced <oddlyspaced1@gmail.com>
2021-05-04 14:49:31 +0530

    launch: Use screen insead of tmux

--- launch.sh
+++ launch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+echo "Killing existing sessions..."
+screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+killall -p firefox
 while IFS= read -r line; do
 	city=$(echo $line | cut -d'=' -f 1)
-	echo "Launching tmux for $city"
-	tmux new-session -d -s $city python covid.py $line
+	echo "Launching for $city"
+	screen -S $city -d -m python3 covid.py $line
 done < $1
